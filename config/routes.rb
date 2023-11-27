ee4ec4d734cc4fc0ded0d4ac6aaa1f8b89783eef
Masayuki Horimoto <snow.smile1201@gmail.com>
2023-11-27 06:14:11 +0000

    本番環境テスト：ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -26,24 +26,24 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root to: 'homes#top'
-    get 'users/:id/my_favorites' => 'users#my_favorites', as: 'my_favorites'
-    get 'users/confirm_withdraw' => 'users#confirm_withdraw', as: 'confirm_withdraw'
-    patch 'users/:id/withdraw' => 'users#withdraw', as: 'withdraw'
-    get '/post/hashtag/:name' => 'posts#hashtag', as: 'hashtag'
     get 'search' => 'searches#search'
     get 'genre_search' => 'searches#genre_search'
-
     resources :users, only: [:index, :show, :edit, :update] do
       member do
         get :followings, :followers
       end
       resource :user_relationships, only: [:create, :destroy]
       end
+    get 'users/:id/my_favorites' => 'users#my_favorites', as: 'my_favorites'
+    get 'users/confirm_withdraw' => 'users#confirm_withdraw', as: 'confirm_withdraw'
+    patch 'users/:id/withdraw' => 'users#withdraw', as: 'withdraw'
+
     resources :children, only: [:index, :create, :show, :edit, :update]
     resources :posts, only: [:index,:new, :create, :show, :edit, :update, :destroy] do
       resources :post_comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
     end
+    get '/post/hashtag/:name' => 'posts#hashtag', as: 'hashtag'
     resources :inform_activities, only: [:index] do
       collection do
         delete :destroy_all
